927a863a6ee092203b11600f56fbbe6f74c97af7
Yizhi Tang <tangyz@umich.edu>
2023-09-08 15:43:43 -0400

    use AWS Cloudscape components

--- schedulegpt_react/src/App.js
+++ schedulegpt_react/src/App.js
@@ -1,56 +1,60 @@
-import React, { useState } from 'react';
+// React
+import * as React from "react";
+// AWS Cloudscape
+import ContentLayout from "@cloudscape-design/components/content-layout";
+import Container from "@cloudscape-design/components/container";
+import Header from "@cloudscape-design/components/header";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import Button from "@cloudscape-design/components/button";
+// ./components
+import TaskAttribtueEditor from "./components/attribute_editor";
 
-import InputColumnLayout from './components/input';
 
-import logo from './logo.svg';
-import remove_icon from './images/icons8-remove-26.png';
-import add_icon from './images/icons8-add-32.png';
-import rocket_icon from './images/icons8-rocket-53.png';
-import enter_icon from './images/icons8-enter-48.png';
-
-
-import './App.css';
-
-function App() {
-  const [boxNumber, setBoxNumber] = useState(1);
-  const [sendData, setSendData] = useState(false);
-
+export default () => {
   return (
-    <div class="grid-container">
-        <h1 id="title" class="grid-item">ScheduleGPT</h1>
-        <InputColumnLayout
-          inputBoxNumber={boxNumber}
-          sendData={sendData}
-          setSendData={setSendData}
-        />
-        <img
-          id="add-button"
-          src={add_icon}
-          onClick={() => {
-            setBoxNumber(boxNumber+1);
-          }}
-        />
+    <ContentLayout
+      header={
+        <SpaceBetween size="m">
+          <Header
+            variant="h1"
+            description="ChatGPT will help you with your schedule"
+          >
+            Schedule GPT
+          </Header>
+        </SpaceBetween>
+      }
+    >
 
-        <img
-          id="launch-button"
-          src={rocket_icon}
-          onClick={() => {
-            setSendData(true);
-          }}
-        /> 
-        
-        <div id="calendar-preview" class="grid-item">
-            Calendar Preview Here
-        </div>
-
-        <div id="chat-box" class="grid-item">
-            <input type = "text" id = "chat-box-text" placeholder = "Describe any changes you want to make" />
-            <a>
-                <img id='enter-button' src={enter_icon}/>
-            </a>
-        </div>
-    </div>
+      <SpaceBetween size="m">
+          <Container
+            header={
+              <Header
+                variant="h2"
+                description="Please enter your tasks one by one"
+              >
+                Adding Tasks
+              </Header>
+            }
+          >
+            <SpaceBetween size="m">
+              <TaskAttribtueEditor />
+              <Button variant="primary">Generate Schedule</Button>
+            </SpaceBetween>
+          </Container>
+          <Container
+            header={
+              <Header
+                variant="h2"
+                description="Please view your schedule here"
+              >
+                View Schedule
+              </Header>
+            }
+          >
+            Calender PlaceHolder
+          </Container>
+      </SpaceBetween>
+      
+    </ContentLayout>
   );
 }
-
-export default App;
